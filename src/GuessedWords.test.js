e9042a07e14d081b62c879749ab9f86264a4a4d2
jonsoku2 <whdtjr2792@gmail.com>
2020-11-19 09:45:30 +0900

    Tests and code for non-zero guessed words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -23,7 +23,7 @@ describe("if there are no words guessed", () => {
     wrapper = setup({ guessedWords: [] });
   });
   test("renders without error", () => {
-    const component = findByTestAttr(wrapper, "component-guessed-word");
+    const component = findByTestAttr(wrapper, "component-guessed-words");
     expect(component.length).toBe(1);
   });
   /**
@@ -33,8 +33,40 @@ describe("if there are no words guessed", () => {
    *  이 방법은 사실상의 부정 또는 역입니다 filter.
    */
   test("renders instructions to guess a word", () => {
+    // console.log(wrapper.debug());
     const instructions = findByTestAttr(wrapper, "component-instructions");
     expect(instructions.text().length).not.toBe(0);
   });
 });
-describe("if there are words guessed", () => {});
+describe("if there are words guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    {
+      guessedWord: "train",
+      letterMatchCount: 3,
+    },
+    {
+      guessedWord: "agile",
+      letterMatchCount: 1,
+    },
+    {
+      guessedWord: "party",
+      letterMatchCount: 5,
+    },
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1)
+  });
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words')
+    expect(guessedWordsNode.length).toBe(1)
+  });
+  test("correct number of guessed words", () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordsNodes.length).toBe(guessedWords.length)
+  });
+});
