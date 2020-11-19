7a00e91630198ec0e7fead0d1affc00c30f25291
jonsoku2 <whdtjr2792@gmail.com>
2020-11-19 11:31:52 +0900

    Complete Input component render for success=false

--- src/Input.test.js
+++ src/Input.test.js
@@ -12,18 +12,24 @@ const setup = (initialState = {}) => {
     .dive();
 };
 
-setup();
-
 describe("render", () => {
   describe("word has not been guessed", () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
     test("renders component without error", () => {
-      //
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     });
     test("renders input box", () => {
-      //
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
     });
     test("renders submit box", () => {
-      //
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
     });
   });
   describe("word has been guessed", () => {
